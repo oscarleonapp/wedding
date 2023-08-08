690ea8bd57f1ad9c89df797943cb1e9cbdda5fa0
oscarleon <oscarleon.alcatel@gmai.com>
2023-08-08 13:09:35 -0600

    nuevo tema

---
+++ GridLayou/script.js
@@ -0,0 +1,15 @@
+function updateCountdown() {
+    const targetDate = new Date("2023-12-31").getTime();
+    const now = new Date().getTime();
+    const timeRemaining = targetDate - now;
+
+    const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
+
+    document.getElementById("countdown").innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s`;
+}
+
+updateCountdown();
+setInterval(updateCountdown, 1000);
